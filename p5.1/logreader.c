1148a14a39d2dc2f60891fb024727ca328a06bf3
jamesvuong2 <23180228@student.curtin.edu.au>
2025-05-14 16:04:57 +0800

    Add display, plot, and logreader functionalities with associated data files

--- p5.1/logreader.c
+++ p5.1/logreader.c
@@ -8,23 +8,26 @@ int main(int argc, char* argv[])
 
     // check if argc >= 3
 
-    FILE *log = fopen(argv[0], "r");
+    FILE *log = fopen(argv[1], "r");
     //FILE *output = fopen(argv[2], "w");
 
-    //if (log == "fail") {
-    //    printf("Contains fail.\n");
-    //    return 2;
-    //}
     if (log == NULL) {
         printf("Error opening file.\n");
         return 1;
     }
-   
     
+    char zMonth[4];
+    int date, hour, minute, seconds;
     char buffer[100];
+    char zProcessName[51];
+    int nRead;
+
     while (!feof(log)) {
-        char buffer = fgets(buffer, sizeof(buffer), log);
-        printf("%d", buffer);  // Print each line from the file
+        fscanf(log, "fail");
+        nRead = fscanf(log, "%3s %d %d:%d:%d %50s", zMonth, &date, &hour, &minute, &seconds, zProcessName);
+        char* c = fgets(buffer, sizeof(buffer), log);
+        printf("%s %d %d:%d:%d %s\n", zMonth, date, hour, minute, seconds, zProcessName);
+        printf("%s", c);  // Print each line from the file
         //fputs(c, output);  // Write each character to the destination file
     }
 
