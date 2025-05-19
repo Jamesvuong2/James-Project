850c4b80afcd8194f9b70e6901c8b5b041eb5a5a
Jamesvuong2 <jamesvuong2@gmail.com>
2025-05-19 11:15:42 +0800

    Refactor display functionality and add example data file

--- p5.1/display.c
+++ p5.1/display.c
@@ -2,84 +2,14 @@
 #include <stdlib.h>
 #include "plot.h"
 
-static int plotActual(double **data, int rows, int cols, char* dataFilename, char* scriptFilename, FILE* dataFile, FILE* scriptFile)
-{
-    int i, j;
-    char command[250];
-
-    for(i = 0; i < rows; i++)
-    {
-        for(j = 0; j < cols; j++)
-        {
-            fprintf(dataFile, "%f ", data[i][j]);
-        }
-        fputc('\n', dataFile);
-    }
-
-    // Write the gnuplot script file.
-    fprintf(scriptFile, 
-        "set pm3d\n"
-        "splot \"%s\" matrix with pm3d notitle\n"
-        "pause -1 \"Press Enter\"\n", 
-        dataFilename);
-
-    // Ensure the data is actually written to disk prior to running gnuplot.
-    fflush(dataFile);
-    fflush(scriptFile);
-
-    // Run gnuplot itself.
-    sprintf(command, "gnuplot %s </dev/tty", scriptFilename);
-    return system(command);
-}
-
-void plot(double **data, int rows, int cols)
-{
-    char dataFilename[] = "/tmp/ucpplotdataXXXXXX";
-    char scriptFilename[] = "/tmp/ucpplotscriptXXXXXX";
-    int dataFd, scriptFd;
-    FILE* dataFile;
-    FILE* scriptFile;
-    int ret;
-
-    // Open temporary files, based on template filenames.
-    dataFd = mkstemp(dataFilename);
-    scriptFd = mkstemp(scriptFilename);
-    if(dataFd == -1 || scriptFd == -1)
-    {
-        perror("Plotting failed - unable to create temporary files");
-    }
-    else
-    {
-        // Convert the OS-level file descriptors to C-level FILE pointers.
-        dataFile = fdopen(dataFd, "w");
-        scriptFile = fdopen(scriptFd, "w");
-        if(dataFile == NULL || scriptFile == NULL)
-        {
-            perror("Plotting failed - unable to open temporary file stream");
-        }
-        else
-        {
-            // Perform the actual plotting.
-            ret = plotActual(data, rows, cols, 
-                             dataFilename, scriptFilename,
-                             dataFile, scriptFile);
-            if(ret % 256 != 0)
-            {
-                perror("Plotting failed - unable to run gnuplot");
-            }
-            fclose(dataFile);
-            fclose(scriptFile);
-        }
-    }
-}
-
 int main(int argc, char* argv[])
 {
-    for (int i = 0; i < argc; i++) {
+        for (int i = 0; i < argc; i++) {
         printf("%s\n", argv[i]);
     }
 
-    // check if argc >= 3
+    int rows, cols;
+    int i, j;
 
     FILE *pf = fopen(argv[1], "r");
 
@@ -88,12 +18,21 @@ int main(int argc, char* argv[])
         return 1;
     }
     
-    //char buffer[100];
+    char buffer[100];
 
-    while (!feof(pf)) {
-        char c = fgetc(pf);
-        printf("%c", c);  // Print each line from the file
-        //fputs(c, output);  // Write each character to the destination file
+    //while (!feof(pf)) {
+    //    double **data = fgetc(pf);
+    //    printf("%f", data[i][j]);  // Print each line from the file
+    //}
+
+    for(i = 0; i < rows; i++)
+    {
+        for(j = 0; j < cols; j++)
+        {
+            double **data = fgets(buffer, sizeof(buffer), pf);
+            printf("%f", data[i][j]);
+        }
+        //fputc('\n', dataFile);
     }
 
     fclose(pf);
