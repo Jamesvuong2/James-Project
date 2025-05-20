ae17e073655f01a8771f1bdd47533c33c26fd977
jamesvuong2 <23180228@student.curtin.edu.au>
2025-05-20 13:59:09 +0800

    Add initial implementation of struct and file I/O for logging topics

---
+++ p6/struct.c
@@ -0,0 +1,46 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "struct.h"
+
+// This is a quick example of file IO complemented with struct allocation and data storing.
+int main() {
+    FILE *fp;
+    Log *list;
+    char temp[10];
+    int logAmount;
+    int i;
+
+    fp = fopen("logfile.txt", "r");
+
+    if (fp == NULL) {
+        perror("cannot open file");
+    } else {
+        // The logfile contains one integer at the beginning to indicate the amount of entries. Each entry contains date and the corresponding C programming topic.
+
+        fscanf(fp, "%d", &logAmount);
+
+        list = (Log *)malloc(sizeof(Log) * logAmount); // Create array of Log struct to store data. 
+
+        if (list == NULL) {
+            perror("Memory allocation failed");
+            fclose(fp);
+            return 1;
+        }
+
+        for (i = 0; i < logAmount; i++) {
+            fscanf(fp, "%d-%d-%d\n", &(list[i].day), &(list[i].month), &(list[i].year)); // Read date 
+            fscanf(fp, "%s %d : ", temp, &(list[i].week)); // Read week 
+            fgets(list[i].topic, 100, fp); // Retrieve the topic and the '\n' char at the end
+        }
+
+        for (i = 0; i < logAmount; i++) {
+            printf("%02d/%02d/%d (Week %d) : %s", list[i].day, list[i].month, list[i].year, list[i].week, list[i].topic);
+        }
+        printf("\n");
+
+        free(list);
+        fclose(fp);
+    }
+
+    return 0;
+}
---
+++ p6/struct.h
@@ -0,0 +1,12 @@
+#ifndef STRUCT_H
+#define STRUCT_H
+
+typedef struct {
+    int day;
+    int month;
+    int year;
+    int week;
+    char topic[100];
+}Log;
+
+#endif
